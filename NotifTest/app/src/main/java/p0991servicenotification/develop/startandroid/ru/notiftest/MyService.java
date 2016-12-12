97717ff735033a71ede33adae2992a9e684e20d5
HennessyRich <denis.bondarenko@ukr.net>
2016-12-12 23:06:46 +0200

    Notifications
    рабочее уведомление, приходит по таймингу.  И даже по клику переводит по
    Uri ссылке

---
+++ NotifTest/app/src/main/java/p0991servicenotification/develop/startandroid/ru/notiftest/MyService.java
@@ -0,0 +1,102 @@
+package p0991servicenotification.develop.startandroid.ru.notiftest;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.IBinder;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MyService extends Service {
+
+    NotificationManager nm;
+    Timer timer;
+    TimerTask tTask;
+    long interval = 5000;
+    public static final int NOTIFICATION_ID = 854;
+    public void onCreate() {
+        super.onCreate();
+        nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        timer = new Timer();
+        schedule();
+    }
+
+    void schedule() {
+        if (tTask != null) tTask.cancel();
+        if (interval > 0) {
+            tTask = new TimerTask() {
+                public void run() {
+                    sendNotification();
+                }
+            };
+            timer.schedule(tTask, 10000, interval);
+        }
+    }
+
+    void sendNotification() {
+
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://developer.android.com/reference/android/app/Notification.html"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        android.support.v4.app.NotificationCompat.Builder builder = new android.support.v4.app.NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.drawable.ic_stat_name);
+
+        // Set the intent that will fire when the user taps the notification.
+        builder.setContentIntent(pendingIntent);
+
+        builder.setAutoCancel(true);
+
+        /**
+         *Build the notification's appearance.
+         * Set the large icon, which appears on the left of the notification. In this
+         * sample we'll set the large icon to be the same as our app icon. The app icon is a
+         * reasonable default if you don't have anything more compelling to use as an icon.
+         */
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+
+        /**
+         * Set the text of the notification. This sample sets the three most commononly used
+         * text areas:
+         * 1. The content title, which appears in large type at the top of the notification
+         * 2. The content text, which appears in smaller text below the title
+         * 3. The subtext, which appears under the text on newer devices. Devices running
+         *    versions of Android prior to 4.2 will ignore this field, so don't use it for
+         *    anything vital!
+         */
+        builder.setContentTitle("BasicNotifications Sample");
+        builder.setContentText("Time to learn about notifications!");
+        //builder.setSubText("Tap to view documentation about notifications.");
+
+        // END_INCLUDE (build_notification)
+
+        // BEGIN_INCLUDE(send_notification)
+        /**
+         * Send the notification. This will immediately display the notification icon in the
+         * notification bar.
+         */
+        NotificationManager notificationManager = (NotificationManager) getSystemService(
+                NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+        // END_INCLUDE(send_notification)
+    }
+
+
+
+
+
+
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
